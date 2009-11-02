bef741875fac448a6a0bac18f23dde3bcec479c1
James Miller <james@jkmillertech.com>
2009-11-02 14:59:32 -0800

    Add HTTP basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,17 @@ class ApplicationController < ActionController::Base
 
   helper :all
   protect_from_forgery
+  
+  before_filter :authenticate
+  
+  protected
+  
+  def authenticate
+    if ENV['enable_auth'] == 1 || APP_CONFIG[:enable_auth] == 1
+      authenticate_or_request_with_http_basic do |username, password|
+        (username == ENV['auth_username'] || username == APP_CONFIG[:auth_username]) && (password == ENV['auth_password'] || password == APP_CONFIG[:auth_password])
+      end
+    end
+  end
 
 end
